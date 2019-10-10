f5f2b749491160f015e79404b3e083f0516a6a37
Andrzej Chorostian <andrzejchorostian@outlook.com>
2019-10-10 18:46:07 +0200

    Add window resizing

--- window.py
+++ window.py
@@ -3,17 +3,18 @@ import scene
 
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 640
-HEX_RADIUS = 60
+HEX_RADIUS = 24
 
-window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
+window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT, caption="1024x640", resizable=True)
 window.scene_o = scene.MenuScene(HEX_RADIUS)
 
 @window.event
 def on_draw():
     window.clear()
     window.scene_o.on_draw()
-    window.width = WINDOW_WIDTH
-    window.height = WINDOW_HEIGHT
+    #window.width = WINDOW_WIDTH
+    #window.height = WINDOW_HEIGHT
+
 
 @window.event
 def on_mouse_press(x, y, button, modifiers):
@@ -24,5 +25,9 @@ def on_mouse_press(x, y, button, modifiers):
 @window.event
 def on_mouse_scroll(x, y, scroll_x, scroll_y):
     window.scene_o.change_hex_radius(scroll_y)
+
+@window.event
+def on_resize(width, height):
+    window.set_caption("{}x{}".format(width, height))
 
 pyglet.app.run()
