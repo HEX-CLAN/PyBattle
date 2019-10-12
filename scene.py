0a3eafdcf1c04f949f454cfd10e20600c25d7eea
crin <37940924+crinuyi@users.noreply.github.com>
2019-10-12 22:09:48 +0200

    added font and colors

--- scene.py
+++ scene.py
@@ -53,44 +53,46 @@ class GameScene:
 class MenuScene:
     def __init__(self, width, height):
         self.goto = MenuScene
+        pyglet.gl.glClearColor(0.1, 0.1, 0.1, 1)
         pyglet.font.add_file("hexagon_cup.ttf")
         pyglet.font.load('HEXAGON cup font')
+        pyglet.font.add_file("CaviarDreams.ttf")
+        pyglet.font.load('Caviar Dreams')
         self.header = pyglet.text.Label(
             'PyBattle',
             font_name='HEXAGON cup font',
-            font_size=50,
+            font_size=70,
             x=width/2,
-            y=height-100,
+            y=height-130,
             anchor_x='center',
-            color=(255, 255, 255, 255))
+            color=(35, 255, 204, 255))
         self.start_button = pyglet.text.Label(
             '⬡ START',
-            font_name='Arial',
+            font_name='Caviar Dreams',
             font_size=24,
             x=width/2,
             y=height-400,
             anchor_x='center',
-            color=(255, 255, 255, 255)
+            color=(35, 255, 204, 255)
         )
         self.settings_button = pyglet.text.Label(
             '⬡ SETTINGS',
-            font_name='Arial',
+            font_name='Caviar Dreams',
             font_size=24,
             x=width/2,
             y=height-450,
             anchor_x='center',
-            color=(255, 255, 255, 255)
+            color=(35, 255, 204, 255)
         )
         self.exit_button = pyglet.text.Label(
             '⬡ EXIT',
-            font_name='Arial',
+            font_name='Caviar Dreams',
             font_size=24,
             x=width/2,
             y=height-500,
             anchor_x='center',
-            color=(255, 255, 255, 255)
+            color=(35, 255, 204, 255)
         )
-        pyglet.gl.glClearColor(0, 0, 0, 0)
 
     def on_mouse_press(self):
         self.goto = GameScene
@@ -101,19 +103,19 @@ class MenuScene:
                                       521, 428,
                                       846, 428,
                                       846, 229)),
-                             ('c4B', (255, 255, 255, 255,
-                                      255, 255, 255, 255,
-                                      255, 255, 255, 255,
-                                      255, 255, 255, 255)))
+                             ('c4B', (35, 255, 204, 255,
+                                      35, 255, 204, 255,
+                                      35, 255, 204, 255,
+                                      35, 255, 204, 255)))
         pyglet.graphics.draw(4, pyglet.gl.GL_LINE_LOOP,
                              ('v2f', (516, 224,
                                       516, 433,
                                       851, 433,
                                       851, 224)),
-                             ('c4B', (255, 255, 255, 255,
-                                      255, 255, 255, 255,
-                                      255, 255, 255, 255,
-                                      255, 255, 255, 255)))
+                             ('c4B', (35, 255, 204, 255,
+                                      35, 255, 204, 255,
+                                      35, 255, 204, 255,
+                                      35, 255, 204, 255)))
         self.header.draw()
         self.start_button.draw()
         self.settings_button.draw()
