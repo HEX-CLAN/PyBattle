3972da5fe8827a37290d17ebd63180308b8d1dfc
Andrzej Chorostian <andrzejchorostian@outlook.com>
2019-10-14 14:58:03 +0200

    Update globals in settings file

--- scene.py
+++ scene.py
@@ -6,10 +6,11 @@ import time
 
 
 class GameScene:
-    def __init__(self,width,height):
+    def __init__(self):
         self.goto = GameScene
-        self.grid = HexagonalGrid(width, height, 30, 20)
-        self.game_time = time.time()
+        self.grid = HexagonalGrid(settings.width, settings.height, 30, 20)
+        if settings.duration:
+            self.game_time = time.time()
         self.batch = pyglet.graphics.Batch()
         pyglet.gl.glClearColor(0.5, 0.5, 0.5, 1)
 
@@ -47,12 +48,13 @@ class GameScene:
 
     def on_draw(self):
         self.batch.draw()
-        time_label = "{:2.2f}".format(time.time() - self.game_time)
-        time_label = pyglet.text.Label(time_label, font_size=20, x=1000, y=10, color=(255,255,255,255))
-        time_label.draw()
+        if settings.duration:
+            time_label = "{:2.2f}".format(time.time() - self.game_time)
+            time_label = pyglet.text.Label(time_label, font_size=20, x=1000, y=10, color=settings.white)
+            time_label.draw()
 
 class MenuScene:
-    def __init__(self,width,height):
+    def __init__(self):
         self.goto = MenuScene
         self.label = pyglet.text.Label('PyBattle', font_name='Times New Roman',
                           font_size=36, x=200, y=200,
