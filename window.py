76cc23d2ce920d8d5058e5a01ef8420c5ed17d45
Andrzej Chorostian <andrzejchorostian@outlook.com>
2019-10-24 09:40:01 +0200

    Refactor

--- window.py
+++ window.py
@@ -6,7 +6,7 @@ import settings
 
 window = pyglet.window.Window(settings.width, settings.height, resizable=True)
 # vsync=False to unlock more fps
-window.scene_o = menuscene.MenuScene()
+window.scene = menuscene.MenuScene()
 if settings.fps:
     window.fps_display = pyglet.window.FPSDisplay(window)
     window.fps_display.label.font_size = 20
@@ -16,29 +16,31 @@ if settings.fps:
 @window.event
 def on_draw(frame_time=0):
     window.clear()
-    window.scene_o.on_draw()
+    window.scene.on_draw()
     if settings.fps:
         window.fps_display.draw()
-
-@window.event
-def on_mouse_press(x, y, button, modifiers):
-    window.scene_o.on_mouse_press()
-
-@window.event
-def on_mouse_release(x, y, button, modifiers):
-    window.scene_o.on_mouse_release()
-    if not isinstance(window.scene_o, window.scene_o.goto):
-        window.scene_o = window.scene_o.goto()
+    if not isinstance(window.scene, window.scene.goto):
+        window.scene = window.scene.goto()
 
 @window.event
 def on_resize(width, height):
     window.set_caption("{}x{}".format(width, height))
     print("{}x{}".format(width, height))
+    window.scene.on_resize(width, height)
 
 @window.event
 def on_mouse_motion(x, y, dx, dy):
-    window.scene_o.on_mouse_motion(x, y)
+    window.scene.on_mouse_motion(x, y)
 
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    print("click down")
+    window.scene.on_mouse_press()
+
+@window.event
+def on_mouse_release(x, y, button, modifiers):
+    print("click up")
+    window.scene.on_mouse_release()
 
 pyglet.clock.schedule_interval(on_draw, 0.0166)
 # 0.001 to unlock more fpx
