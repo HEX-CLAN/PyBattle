4d4c78bae3682b00189c0bc10d71bb06661e1cb9
crin <37940924+crinuyi@users.noreply.github.com>
2019-11-03 00:53:05 +0100

    added SETTINGS and EXIT buttons events
    (weird glitch on settings button)

--- menuscene.py
+++ menuscene.py
@@ -1,12 +1,8 @@
 from kivy.app import App
-from kivy.lang import Builder
-
-kv = Builder.load_file('menuscene.kv')
 
 
 class MenuScene(App):
-    def build(self):
-        return kv
+    pass
 
 
 if __name__ == '__main__':
