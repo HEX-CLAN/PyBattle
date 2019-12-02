6c16252f3f1b184e979c122c2a7c750e66b88f99
crin <37940924+crinuyi@users.noreply.github.com>
2019-12-02 16:58:26 +0100

    previous settings design restored

--- app/scenes/settingsscene.py
+++ app/scenes/settingsscene.py
@@ -1,46 +1,6 @@
-import json
-
-from kivy import Config
-from kivy.uix.label import Label
-from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen
 
 
 class SettingsScene(Screen):
-    json_settings = json.dumps([
-        {
-            "type": "numeric",
-            "title": "Width",
-            "section": "General",
-            "key": "width"
-        },
-        {
-            "type": "numeric",
-            "title": "Height",
-            "section": "General",
-            "key": "height"
-        }
-    ])
-
     def on_enter(self):
         print("Settings")
-
-    def update_changes(self, key, value):
-        if key == "width":
-            if int(value) < 1024 or int(value) > 1920:
-                wrong_value_popup = Popup(title='Wrong value',
-                                          content=Label(text='Enter a value between 1024 and 1920.'),
-                                          size_hint=(None, None), size=(400, 200))
-                wrong_value_popup.open()
-            # else:
-            #     Config.set('graphics', 'width', value)
-            #     Config.write()
-        elif key == "height":
-            if int(value) < 768 or int(value) > 1080:
-                wrong_value_popup = Popup(title='Wrong value',
-                                          content=Label(text='Enter a value between 768 and 1080.'),
-                                          size_hint=(None, None), size=(400, 200))
-                wrong_value_popup.open()
-            # else:
-            #     Config.set('graphics', 'width', value)
-            #     Config.write()
--- app/window.py
+++ app/window.py
@@ -1,32 +1,16 @@
 from kivy.app import App
-from kivy.config import Config, ConfigParser
+from kivy.config import Config
 import scenes.gamescene
 import scenes.menuscene
 import scenes.settingsscene
 
-from scenes.settingsscene import SettingsScene
 
 Config.set('modules', 'monitor', '') #FPS meter
 
 
 class Window(App):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.use_kivy_settings = False
-        self.settings_scene = SettingsScene()
-
-    def build_config(self, config):
-        config.setdefaults("General", {'width': 768, 'height': 1024})
-
-    def build_settings(self, settings):
-        user_config = ConfigParser()
-        user_config.read('window.ini')
-        settings.add_json_panel("Settings", user_config, data=self.settings_scene.json_settings)
-
-    def on_config_change(self, config, section, key, value):
-        self.settings_scene.update_changes(key, value)
-        value = 6
-        print(value)
+    def build(self):
+        pass
 
 
 if __name__ == '__main__':
