abb5c29ed41c361b47a9cb4a230f5648694a2071
Andrzej Chorostian <andrzejchorostian@outlook.com>
2020-01-09 23:28:48 +0100

    Revert "Revert "dirty commit 1""
    This reverts commit be5ad0acaf480d160eab673f5a050bb47d274e60.

--- app/utils/generator.py
+++ app/utils/generator.py
@@ -1,28 +1,17 @@
 import random
+from pybattle.utils import tile
 
-from utils import tile
-# SETUP
-
-# seed = 42352336
-# width = 30
-# height = 20
-# water = 10 # min, max
-
-# INIT
-
-# random.seed(a=seed, version=2)
-# map = [[0] * height for i in range(width)]
 
 # RANDOM GENERATOR
 def set_value(map, value, x, y, width, height, max_diff):
     map[x][y].set_depth(value)
     if value > 0:
-        if x%2 == 1:
+        if x % 2 == 1:
             nearby = ((x-1, y), (x-1, y+1), (x, y-1), (x, y+1), (x+1, y), (x+1, y+1))
         else:
             nearby = ((x-1, y-1), (x-1, y), (x, y-1), (x, y+1), (x+1, y-1), (x+1, y))
         for n in nearby:
-            if n[0] >= 0 and n[0] < width and n[1] >= 0 and n[1] < height and map[n[0]][n[1]].depth == 0:
+            if width > n[0] >= 0 and 0 == map[n[0]][n[1]].depth and 0 <= n[1] < height:
                 v = random.randrange(value-max_diff, value)
                 if v < 0: 
                     v = 0
@@ -35,9 +24,9 @@ def generate_map(width, height, seed, water, max_diff):
 
     for x in range(width):
         for y in range(height):
-            grid[x][y] = tile.Tile()
+            grid[x][y] = tile.Tile((x, y))
 
-    for r in range(water):
+    for r in range(int(water)):
         x = random.randrange(0, width)
         y = random.randrange(0, height)
         set_value(grid, 5, x, y, width, height, max_diff)
@@ -48,14 +37,3 @@ def generate_map(width, height, seed, water, max_diff):
         y = random.randrange(0, height)
         set_value(grid, 2, x, y, width, height, max_diff)
     return grid
-
-
-# PRINT
-
-# for a in range(40):
-#     for b in range(15):
-#         if a%2 == 0:
-#             print(map[b*2][a//2], "    ", end='')
-#         else:
-#             print("   "+str(map[b*2+1][a//2])+"  ", end='')
-#     print()
---
+++ pybattle/__main__.py
@@ -0,0 +1,37 @@
+from pybattle.run import Run
+from kivy.core.window import Window
+from kivy import Config
+
+
+if __name__ == '__main__':
+    # Config.set('graphics', 'fullscreen', 'auto')
+    # Config.set('graphics', 'resizable', 1)
+    # Config.write()
+
+    Config.set('graphics', 'fullscreen', False) # to nadpisuje config, ktory gra wlaczylaby domyslnie
+    Config.write()
+
+    Window.fullscreen = 'auto' # a to ustawia na chama fullscreena, True tez moze zadziala, ale polecam auto
+
+
+
+
+    #
+    # w = Window.size[0]
+    # h = Window.size[1]
+
+    # print("window: ", Window.size)
+    # Config.set('graphics', 'width', w)
+    # print("window: ", Window.size)
+    # Config.set('graphics', 'height', h)
+    # print("window: ", Window.size)
+    #Config.write()
+
+    # print("window:", Window.size)
+    # print("config: (" + Config.get('graphics', 'width') + ", " + Config.get('graphics', 'height') + ")")
+    r = Run()
+
+    r.run()
+
+    # pecet pokazuje 5120x2880 a ustawienie jest 1440x900-HIDPI
+    # lapek pokazuje 2880x1800 a ustawienie jest 1440x900-HIDPI
